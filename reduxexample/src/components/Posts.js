5765b3b3dbe19b9d8c28d7c3539929f9445e2099
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-21 19:24:31 +0530

    updated App.js

--- reduxexample/src/components/Posts.js
+++ reduxexample/src/components/Posts.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 class Posts extends Component {
+    componentWillMount(){
+        console.log(123);
+    }
+
+
     render() {
         return (
             <div>
-                
+                <h1>Posts</h1>
             </div>
         )
     }
