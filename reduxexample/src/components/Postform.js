1fe6f5acbde17640c6c938331e12d80ea99bc8fe
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-21 21:42:10 +0530

    Update Postform.js

--- reduxexample/src/components/Postform.js
+++ reduxexample/src/components/Postform.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react'
 class PostForm extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            title: '',
+            body: ''
+        }
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]: e.target.value});
+    }
+
     render() {
         return (
             <div>
@@ -7,11 +20,12 @@ class PostForm extends Component {
                <form>
                    <div>
                        <label>Title: </label><br/>
-                       <input type = 'text' name='title'/>
+                       <input type = 'text' name='title' value={this.state.title} onChange={this.onChange}/>
                    </div>
+                   <br/>
                    <div>
                        <label>Body: </label><br/>
-                       <textarea name = 'body' />
+                       <textarea name = 'body' value={this.state.body} onChange={this.onChange}/>
                    </div>
                    <br/>
                    <button type='submit'>Submit</button>
