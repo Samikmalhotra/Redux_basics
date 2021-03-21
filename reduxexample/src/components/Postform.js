0102d2afa260d91fe227383318c1c1b0d7dadb70
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-21 21:23:50 +0530

    Update Postform.js

--- reduxexample/src/components/Postform.js
+++ reduxexample/src/components/Postform.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-class Postform extends Component {
+class PostForm extends Component {
     render() {
         return (
             <div>
-                
+               <h1>Add Post</h1> 
             </div>
         )
     }
 }
 
 
-export default  Postform;
+export default  PostForm;
